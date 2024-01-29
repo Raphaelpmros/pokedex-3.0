3afe4f702fc0cd8c11197a7ce5531c2f28591dd5
Raphael Mello <raphaelpmros@hotmail.com>
2024-01-29 15:56:38 -0300

    Deixando o input funcional quando digitamos o nome ou número do pokemon

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,5 +1,8 @@
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search')
 const pokemonName = document.querySelector('.pokemon_name');
-const pokemonNumber = document.querySelector('pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+const pokemonNumber = document.querySelector('.pokemon_number');
 
 
 
@@ -14,5 +17,12 @@ const fetchPokemon = async (pokemon) => {
 
 const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
-
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 }
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value)
+})
